070aefed618178f3b792f00a78cd6d69026aaf15
Betelhem <betelhemsintayehu10@gmail.com>
2025-04-17 17:04:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/header.jsx";
 import Footer from "./components/footer/footer.jsx";
+import EasySteps from "./components/easySteps/easySteps.jsx";
+
 
 function App() {
   return (
     <div className="App">
       <h1>React app test</h1>
       <Header />
+       
+       <EasySteps/>
+
       <Footer />
     </div>
   );
---
+++ src/components/easySteps/easySteps.jsx
@@ -0,0 +1,46 @@
+import './easySteps.css'
+import "@fontsource/poppins/200.css";
+import "@fontsource/philosopher/700.css"; // Bold
+
+
+
+function EasySteps(){
+return(
+    <div className="easy-steps">
+        <p className='steps'>EASY STEPS</p>
+        <h2 className='detail'>Details about process</h2>
+        <div className="processes">
+            <div className="process1">
+                <p className='number'>01.</p>
+                <h2>Comprehensive financial assessment</h2>
+                <p className='process-detail'>Understanding the client's current financial standing,
+                     goals, and challenges lays the foundation for tailored accounting solutions.
+                </p>
+            </div>
+            <div className="process2">
+                <p className='number'>02.</p>
+                <h2>Customized financial planning</h2>
+                <p className='process-detail'>A well-crafted plan should not only address
+                     immediate concerns but also provide a roadmap for long-term success.
+                </p>
+            </div>
+            <div className="process3">
+                <p className='number'>02.</p>
+                <h2>Customized financial planning</h2>
+                <p className='process-detail'>This step is crucial for compliance, decision-making, 
+                    and maintaining a transparent view of the client's status.
+                </p>
+            </div>
+            <div className="process4">
+                <p className='number'>04.</p>
+                <h2>Continuous monitoring and advisory services</h2>
+                <p className='process-detail'>Financial landscapes evolve, and it's essential to provide ongoing support. 
+                    Regularly monitor financial activities and goals.
+                </p>
+            </div>
+
+        </div>
+    </div>
+)
+}
+export default EasySteps;
