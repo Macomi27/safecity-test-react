7c0389bf9f962517abd22c2b8a402832c9c61696
Macomi27 <macomfortitcham@gmail.com>
2025-04-17 10:19:37 +0100

    setting up header and footer components

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+        <h1>My footer</h1>
+        </footer>
+    );
+}
+
+export default Footer;
