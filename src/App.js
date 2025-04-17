6f5e31fb36ee2c412986328cc14e7b8b5c4e1654
Ayomide Aina <joytife8@gmail.com>
2025-04-17 22:53:58 +0100

    added components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/header.jsx";
 import Footer from "./components/footer/footer.jsx";
+import People from "./components/people/People.jsx";
 
 function App() {
   return (
     <div className="App">
       <h1>React app test</h1>
       <Header />
+      <People />
       <Footer />
     </div>
   );
