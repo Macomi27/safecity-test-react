cd9d4699604aef096f51d4866673b35fa46c3eed
Yetunde-Iyanu <awoladeyetunde60@gmail.com>
2025-04-18 09:21:07 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/header.jsx";
 import Footer from "./components/footer/footer.jsx";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ServicesSection from "./components/ServicesSection.jsx";
+import Economic from "./components/Economic.jsx";
+import Tax from "./components/Tax.jsx";
+import HRPayroll from "./components/HRPayroll.jsx";
+import Accounting from "./components/Accounting.jsx"; 
 
 function App() {
   return (
-    <div className="App">
-      <h1>React app test</h1>
+    <Router>
       <Header />
+      <Routes>
+        <Route path="/" element={<ServicesSection />} />
+        <Route path="/economic" element={<Economic />} />
+        <Route path="/tax" element={<Tax />} />
+        <Route path="/hr-payroll" element={<HRPayroll />} />
+        <Route path="/accounting" element={<Accounting />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
